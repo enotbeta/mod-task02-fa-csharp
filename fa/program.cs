3addc89d8b90475266e695fba3e2a24cac033ba9
Sergey Kamskov <sk1999@inbox.ru>
2021-03-21 13:56:57 +0300

    b24

--- fa/program.cs
+++ fa/program.cs
@@ -16,17 +16,121 @@ namespace fans
 
   public class FA1
   {
+      public static State a = new State()
+        {
+            Name = "a",
+            Transitions = new Dictionary<char, State>(),
+            IsAcceptState = false,
+        };
+        public static State b = new State()
+        {
+            Name = "b",
+            Transitions = new Dictionary<char, State>(),
+            IsAcceptState = false,
+        };
+        public static State c = new State()
+        {
+            Name = "c",
+            Transitions = new Dictionary<char, State>(),
+            IsAcceptState = true,
+        };
+        public static State d = new State()
+        {
+            Name = "d",
+            Transitions = new Dictionary<char, State>(),
+            IsAcceptState = false,
+        };
+        public static State e = new State()
+        {
+            Name = "e",
+            Transitions = new Dictionary<char, State>(),
+            IsAcceptState = false,
+        };
+        State InitialState = a;
+        public FA1()
+        {
+            a.Transitions['0'] = d;
+            a.Transitions['1'] = b;
+            b.Transitions['1'] = b;
+            b.Transitions['0'] = c;
+            c.Transitions['0'] = e;
+            c.Transitions['1'] = c;
+            d.Transitions['0'] = e;
+            d.Transitions['1'] = c;
+            e.Transitions['0'] = e;
+            e.Transitions['1'] = e;
+        }
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+        State current = a;
+        foreach (var c in s)
+        {
+            Console.WriteLine(current.Name);
+            current = current.Transitions[c]; 
+            if (current == e) 
+            {return false;}
+            if (current == null)
+                    return null;
+            
+        }
+        Console.WriteLine(current.Name);
+        return current.IsAcceptState;
+        
     }
   }
 
   public class FA2
   {
+      public static State a = new State()
+        {
+            Name = "a",
+            Transitions = new Dictionary<char, State>(),
+            IsAcceptState = false,
+        };
+        public static State b = new State()
+        {
+            Name = "b",
+            Transitions = new Dictionary<char, State>(),
+            IsAcceptState = true,
+        };
+        public static State c = new State()
+        {
+            Name = "c",
+            Transitions = new Dictionary<char, State>(),
+            IsAcceptState = false
+        };
+        public static State d = new State()
+        {
+            Name = "d",
+            Transitions = new Dictionary<char, State>(),
+            IsAcceptState = false
+        };
+        public FA2()
+        {
+            a.Transitions['0'] = d;
+            a.Transitions['1'] = b;
+            b.Transitions['1'] = a;
+            b.Transitions['0'] = c;
+            c.Transitions['0'] = b;
+            c.Transitions['1'] = d;
+            d.Transitions['0'] = a;
+            d.Transitions['1'] = c;
+
+        }
     public bool? Run(IEnumerable<char> s)
     {
-      return false;
+      State current = b;
+        foreach (var c in s)
+        {
+            Console.WriteLine(current.Name);
+            current = current.Transitions[c];
+            
+           
+            if (current == null)
+                    return null;
+            
+        }
+        return current.IsAcceptState;
     }
   }
 
@@ -34,7 +138,7 @@ namespace fans
   {
     static void Main(string[] args)
     {
-      String s = "01111";
+      String s = "1111111111101111111111011111111111111";
       FA1 fa1 = new FA1();
       bool? result1 = fa1.Run(s);
       Console.WriteLine(result1);
